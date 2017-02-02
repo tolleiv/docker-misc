a05a4e2abdb7891b4a8fdfd04d47781c70ea45ce
tolleiv <github@tolleiv.de>
2017-02-02 22:07:10 +0100

    Remove inotifywait

--- 2017-prometheus-reload/refresh.sh
+++ 2017-prometheus-reload/refresh.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-while true; do
-   inotifywait "$(readlink -f $1)"
-   echo "[$(date +%s)] Trigger refresh"
-   curl -sSL -X POST "$2" > /dev/null
-done
+FILE=$1
+URL=$2
+HASH=$(md5sum $(readlink -f $FILE))
 
+while true; do
+   NEW_HASH=$(md5sum $(readlink -f $FILE))
+   if [ "$HASH" != "$NEW_HASH" ]; then
+     HASH="$NEW_HASH"
+     echo "[$(date +%s)] Trigger refresh"
+     curl -sSL -X POST "$2" > /dev/null
+   fi
+   sleep 5
+done
