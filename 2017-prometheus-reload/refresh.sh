bc2c0c6833daf69f31e067b8d1822a82505b638d
Tolleiv Nietsch <tolleiv.nietsch@aoe.com>
2017-02-02 14:02:36 +0100

    Make sure we follow symlinks

--- 2017-prometheus-reload/refresh.sh
+++ 2017-prometheus-reload/refresh.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-inotifywait -r -q -m -e close_write "$1" | while read -r filename event; do echo "[$(date +%s)] Trigger refresh"; curl -sSL -X POST "$2" > /dev/null; done
+inotifywait -r -q -m -e close_write "$(readlink -f $1)" | while read -r filename event; do echo "[$(date +%s)] Trigger refresh"; curl -sSL -X POST "$2" > /dev/null; done
 
 
